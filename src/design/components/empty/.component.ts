882d6ca33fc703b396133b8ce6665cedfc595425
Robson Pinto <bissaum@gmail.com>
2024-09-01 22:50:15 -0300

    fix(card): full click

--- src/design/components/empty/.component.ts
+++ src/design/components/empty/.component.ts
@@ -1,13 +1,24 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Pages } from '@/app/.types';
 
 @Component({
   selector: 'ui-empty',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './.html',
   styleUrl: './.scss'
 })
 export class EmptyComponent {
   @Input() heading = '';
   @Input() text = '';
+  @Input() link: Pages | undefined;
+  @Input() button: string | undefined;
+
+  constructor(private router: Router) {}
+
+  goTo(item: Pages) {
+    this.router.navigate([item]);
+  }
 }
